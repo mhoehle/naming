8791f84d369386d2698ce3655d30ae4e32f3f8ba
hoehleatsu <hoehle@lillagubben>
2017-04-29 00:41:58 +0200

    2nd try with hard links

--- R/dataio.R
+++ R/dataio.R
@@ -1,1 +1,75 @@
-../../R/dataio.R
+######################################################################
+## Author: Michael Höhle <http://www.math.su.se/~hoehle>
+## Date:   2017-04-28
+##
+## R code to download and read the 2015 ONS baby names data from the
+## ONS web site. The ONS data only contains names with more than two
+## uses in the year. As a consequence, an inquiry with the ONS
+## customer support was made to get the number of names occurring once
+## and twice for girls and boys. We add these as artificial names to
+## the regular ONS dataset.
+######################################################################
+
+library("dplyr")
+library("readxl")
+library("magrittr")
+##Which year to analyze
+year <- 2015
+
+##Download (if necessary) and load boys and girls tables in order to
+##join them into one table
+newborn_not12 <- bind_rows(
+  lapply( c(girls="girls",boys="boys"), function(sex) {
+    ##Load CSV file
+    destfile <- file.path("..","Data",paste0("2015",sex,"namesfinal.xls"))
+    if (!file.exists(destfile)) {
+      download.file(paste0("https://www.ons.gov.uk/file?uri=/peoplepopulationandcommunity/birthsdeathsandmarriages/livebirths/datasets/babynamesenglandandwalesbabynamesstatistics",sex,"/",year,"/",filename),destfile=destfile)
+    }
+    df <- read_excel(path=destfile,sheet="Table 6",skip=4)
+    ##Remove empty rows and convert types
+    df <- df %>% filter(!is.na(Name)) %>%
+      mutate(Count=as.numeric(Count), Rank=as.numeric(Rank), Sex=sex, Name=gsub(" ", "", Name, fixed = TRUE))
+    return(df)
+  })
+)
+
+##Count totals for boys and girls in the 2015 data. Note: These data
+##do not contain names with just 1 or 2 uses in that year
+total_not12 <- newborn_not12 %>% group_by(Sex) %>% summarise(Count_not12=sum(Count))
+total_not12
+
+##Total number of boys & girls with names in the UK and Wales
+##according to the textual info in the Sheet 6 of each Excel file
+total <- data.frame(sex2=c("boys","girls"), Count=c(358136 - 14,339716 - 10))
+
+##Deduce what's missing
+missing <- bind_cols(total,total_not12) %>% mutate(missing=Count - Count_not12)
+missing %>% select(Sex,missing)
+
+##Data from Email VSOB customer service team (vsob@ons.gsi.gov.uk) on 2017-02-15
+##names12 <- read.csv(file=file.path("..","Data","names-1or2occurrences.csv"))
+names12 <- read.csv(textConnection("What,Sex,Factor,Count
+names_occurring_twice,boys,2,3050
+names_occurring_once,boys,1,18741
+names_occurring_twice,girls,2,3999
+names_occurring_once,girls,1,23733"
+))
+
+names12 <- names12 %>% mutate(TotalCount = Factor * Count)
+names12
+names12 %>% group_by(Sex) %>% summarise(n=sum(TotalCount))
+
+newborn_12 <- names12 %>% group_by(Sex, Factor) %>% do( {
+  data.frame( Rank=1e9, Name=paste0(toupper(.$Sex),"#",sprintf("%.5d",seq_len(.$Count)),"#f",.$Factor))
+}) %>% rename(Count = Factor)
+
+newborn <- bind_rows(newborn_not12, newborn_12)
+##Check for doubles!!
+stopifnot(newborn %>% group_by(Name,Sex) %>% summarise(n=n()) %>% filter(n>1) %>% nrow() == 0)
+
+##Deduce empirical proportions per sex
+newborn <- newborn %>% group_by(Name,Sex) %>% summarise(Count=sum(Count)) %>%
+  mutate(p=Count/sum(Count))
+
+##Store resulting data.frame for further analysis in the Rmd of the article
+save(file=file.path("..","Data","newborn.RData"),list=c("newborn"))
--- R/ons-ts-collision.R
+++ R/ons-ts-collision.R
@@ -1,1 +1,107 @@
-../../R/ons-ts-collision.R
+######################################################################
+## Author: Michael Höhle <http://www.math.su.se/~hoehle>
+## Date:   2017-04-23
+##
+## Description:
+##  Create bonus material plot containing the time series of the UK baby
+##  name collision probability.
+######################################################################
+
+library("readxl")
+library("dplyr")
+library("ggplot2")
+##devtools::install_github("hoehleatsu/birthdayproblem")
+library("birthdayproblem")
+
+######################################################################
+## ONS Data files have to be manually downloaded from
+## https://www.ons.gov.uk/peoplepopulationandcommunity/birthsdeathsandmarriages/livebirths/datasets/babynamesenglandandwalesbabynamesstatisticsgirls
+## and
+## https://www.ons.gov.uk/peoplepopulationandcommunity/birthsdeathsandmarriages/livebirths/datasets/babynamesenglandandwalesbabynamesstatisticsboys. Alternatively,
+## one can run the following R code:
+######################################################################
+filenames <- readLines("filenames.txt")
+for (filename in filenames) {
+  ##Extract year and sex
+  sex <- gsub("^([0-9]{4})(girls|boys).*","\\2",filename)
+  year <- gsub("^([0-9]{4})(girls|boys).*","\\1",filename)
+  ##Download the file if it doesn't exist
+  destfile <- file.path("..","Data",filename)
+  if (!file.exists(destfile)) {
+    download.file(paste0("https://www.ons.gov.uk/file?uri=/peoplepopulationandcommunity/birthsdeathsandmarriages/livebirths/datasets/babynamesenglandandwalesbabynamesstatistics",sex,"/",year,"/",filename),destfile=destfile)
+  }
+}
+
+##Loop over all available files in the Data2 directory. Some warnings
+##are to be expected, because the excel file contains text passages at
+##the end informing about special removals etc.
+names <- NULL
+for (sex in c("Girls","Boys")) {
+  files <- list.files(file.path("..","Data"), pattern=paste0("[0-9]{4}",tolower(sex)))
+  for (i in seq_len(length(files))) {
+    name <- files[i]
+    filePath <- file.path("..","Data",name)
+    ##Find name of the sheet containing the NAME & COUNT information
+    sheets <- readxl::excel_sheets(path=filePath)
+    sheetName <- grep(paste0(sex," names - E&W"),sheets, value=TRUE)
+    if (length(sheetName) == 0) {
+      sheetName <- tail(grep("Table [0-9]",sheets, value=TRUE),n=1)
+    }
+
+    x <- readxl::read_excel(path=filePath,sheet=sheetName,skip=4)
+    x <- x %>% filter(!is.na(Rank)) %>% mutate(year=substr(name,1,4),sex=tolower(sex))
+    names <- rbind(names, x)
+  }
+}
+
+##Add empirical relative frequencies per year
+names$Name <- gsub("[ ]+$","",names$Name)
+names <- names %>% group_by(year) %>% mutate(p = Count/sum(Count))
+
+######################################################################
+##Compute and visualize the collision probablity
+######################################################################
+
+## Compute collision prob for different group sizes
+collision <- names %>% group_by(year) %>% do({
+  n <- 27L
+  p <- sapply(n, function(n) birthdayproblem::pbirthday_up(n=n, .$p ,method="mase1992")$prob)
+  data.frame(n=n, p=p)
+})
+
+##Make the time series plot
+p <- ggplot(collision, aes(x=as.numeric(year),y=p)) + geom_line(size=1.2,color="lightsalmon2") + xlab("Time (year)") + ggtitle("Probability of a name clash in a group of 27 kids born in year YYYY in the UK and Wales") + ylab("Probability")+  scale_y_continuous(limits=c(0,1),labels=scales::percent) +  scale_colour_discrete(name  ="n") + geom_point(data=data.frame(year=2015,p=0.458),color="lightsalmon4") + scale_x_continuous(breaks=seq(min(as.numeric(collision$year)),max(as.numeric(collision$year)),2))
+## Transparent background in ggplot - see http://stackoverflow.com/questions/31550435/issue-saving-r-plot-with-transparent-background
+p + theme_bw() + theme(plot.background = element_rect(fill = "transparent",colour = NA))
+
+##Store to file.
+ggsave(filename="timeseries.png", dpi=300, width=8, height=4, bg = "transparent")
+
+
+######################################################################
+## Word clouds
+######################################################################
+
+require("wordcloud")
+source("mywordcloud.R")
+
+names2015 <- names %>% filter(year==2015)
+boys2015 <- names %>% filter(year==2015, sex == "boys")
+girls2015 <- names %>% filter(year==2015, sex == "girls")
+
+set.seed(123)
+pdf(file="wordcloud-girls.pdf",width=10,height=10)
+par(mar=c(0,0,0,0))
+pal <- brewer.pal(9, "PuRd")[-c(1:2)]
+wordcloud(girls2015$Name,girls2015$Count,colors=pal,min.freq=50,random.order=FALSE)
+dev.off()
+pdf(file="wordcloud-boys.pdf",width=10,height=10)
+par(mar=c(0,0,0,0))
+pal <- brewer.pal(9, "Blues")[-c(1:2)]
+wordcloud(boys2015$Name,boys2015$Count,colors=pal,min.freq=50,random.order=FALSE)
+dev.off()
+
+
+
+
+
